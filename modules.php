7602c51499a9643f5effaf0d748af12b4dc0059a
Dave Tesch <dev@hockeydev.com>
2025-10-19 09:34:19 -0500

    flip card

---
+++ modules.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="/css/trc.css">
+	<title>TRC</title>
+</head> 
+<body>
+
+
+<main id="subpage">
+
+
+  <div class="container" style="margin-top: 50px;">
+
+
+    <div class="module portfolio-flipcard">
+      <div class="content">
+          <div class="front">
+            <div class="image">
+                
+            </div>
+            
+          </div>
+          <div class="back">
+            Back!
+          </div>
+        </div>
+    </div>
+
+
+  </div>
+
+
+</main>
+
+
+    <script>window.jQuery || document.write('<script src="/js/vendor/jquery-3.7.1.min.js"><\/script>')</script>
+		<script src="/js/bootstrap.min.js"></script>
+		<script src="/js/trc2022.js"></script>
+		<script src="/js/vendor/owl.carousel.min.js"></script>
+		
+		<script type="text/javascript">
+		$('.news-carousel').owlCarousel(
+			{
+			autoplay: true,
+			loop: true,
+			margin: 25,
+			items: 2,
+			dots: true,
+			autoPlay: true,
+			autoplayTimeout: 8000,
+			autoplayHoverPause: true,
+			paginationSpeed : 2000, //how long does it take to physically move?
+			responsive : {
+				1024 : {
+						items: 4
+					}
+				}
+			}
+		);
+		</script>
+		
+</body>
+</html>
